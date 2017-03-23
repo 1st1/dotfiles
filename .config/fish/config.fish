c4c3f6a7f65c9f647d0b70b301e6b00665ed56e8
Yury Selivanov <yury@magic.io>
2017-03-23 13:25:19 -0400

    fish: Detect if vex is missing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,11 +9,13 @@ if test -n "$VIRTUAL_ENV"
     echo "{"(basename $VIRTUAL_ENV)"}" (prompt_pwd)
   end
 else
-  if test -d "/opt/local/bin"
+  if test -d /opt/local/bin
     # MacPorts
     set -xg PATH /opt/local/bin /opt/local/sbin $PATH
   end
-  set -xg PATH $PATH ~/dev/sys-venvs/fish/bin
+  if test -d ~/dev/sys-venvs/fish/bin
+    set -xg PATH $PATH ~/dev/sys-venvs/fish/bin
+  end
 
   function fish_title
     echo (prompt_pwd)
@@ -74,36 +76,45 @@ if test -e ~/.config/fish/local.fish
   source ~/.config/fish/local.fish
 end
 
-function vex
-  # Enhances 'vex' command with an '--wd' command option and
-  # returns you back to the dir you were working in before
-  # entering a venv.
 
-  # Use --wd within a virtual env to set a "home dir" for it.
+if not type -q "vex"
+  function vex
+    echo "Vex is missing."
+    echo "Make ~/dev/sys-venvs/fish venv and install vex there."
+    return 1
+  end
+else
+  function vex
+    # Enhances 'vex' command with an '--wd' command option and
+    # returns you back to the dir you were working in before
+    # entering a venv.
 
-  pushd $PWD
+    # Use --wd within a virtual env to set a "home dir" for it.
 
-  if test (count $argv) -eq 1
-    switch $argv[1]
-    case "--wd"
-      if test -z "$VIRTUAL_ENV"
-        echo "You must be in a virtual env to execute 'vex --wd'"
-        popd
-        return 1
-      else
-        echo $PWD >"$VIRTUAL_ENV/.wd"
-        popd
-        return 0
-      end
-    case "*"
-      set -l wd "$WORKON_HOME/"$argv[1]"/.wd"
-      if test -e $wd
-        read wd <$wd
-        cd $wd
+    pushd $PWD
+
+    if test (count $argv) -eq 1
+      switch $argv[1]
+      case "--wd"
+        if test -z "$VIRTUAL_ENV"
+          echo "You must be in a virtual env to execute 'vex --wd'"
+          popd
+          return 1
+        else
+          echo $PWD >"$VIRTUAL_ENV/.wd"
+          popd
+          return 0
+        end
+      case "*"
+        set -l wd "$WORKON_HOME/"$argv[1]"/.wd"
+        if test -e $wd
+          read wd <$wd
+          cd $wd
+        end
       end
     end
-  end
 
-  command vex $argv
-  popd
+    command vex $argv
+    popd
+  end
 end
