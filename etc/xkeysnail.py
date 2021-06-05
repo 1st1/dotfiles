a91549f811e383f5de656288e1d2d394abbbe4f4
Yury Selivanov <yury@edgedb.com>
2021-06-05 15:31:28 -0700

    More linux updates

--- etc/xkeysnail.py
+++ etc/xkeysnail.py
@@ -8,9 +8,13 @@ define_timeout(1)
 define_keymap(None, {
     K("Super-c"): K("C-Insert"),
     K("Super-v"): K("Shift-Insert"),
+    K("Super-EQUAL"): K("C-EQUAL"),
+    K("Super-MINUS"): K("C-MINUS"),
     **{
+        # No "d" -- we want both ctrl+d and super+d work in terminal
+        # No "c" and "v" -- they are handled specially
         K(f"Super-{ch}"): K(f"C-{ch}")
-        for ch in {'a', 'b', 'd', 'e', 'f', 'g', 'h',
+        for ch in {'a', 'b', 'e', 'f', 'g', 'h',
                    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                    'q', 'r', 's', 't', 'u', 'w', 'x',
                    'y', 'z'}
@@ -19,12 +23,13 @@ define_keymap(None, {
     K("Super-ENTER"): K("C-ENTER"),
     K("Super-RIGHT_BRACE"): K("C-RIGHT_BRACE"),
     K("Super-LEFT_BRACE"): K("C-LEFT_BRACE"),
-    K("Super-UP"): K("PAGE_UP"),
-    K("Super-DOWN"): K("PAGE_DOWN"),
+    K("Super-UP"): K("C-HOME"),
+    K("Super-DOWN"): K("C-END"),
     K("Super-LEFT"): K("HOME"),
     K("Super-RIGHT"): K("END"),
     K("Super-Shift-LEFT"): K("Shift-HOME"),
     K("Super-Shift-RIGHT"): K("Shift-END"),
     K("Super-Shift-P"): K("C-Shift-P"),
+    K("Super-Shift-T"): K("C-Shift-T"),
 })
 
